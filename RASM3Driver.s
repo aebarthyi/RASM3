1cba0fe8a4fb7736bf1059674e872888da93d306
Andrew <sonic757RAGEquit@gmail.com>
2020-10-23 15:22:27 -0700

    added string_index_1 function and modified driver code

--- RASM3Driver.s
+++ RASM3Driver.s
@@ -11,12 +11,28 @@
 .equ KBSIZE, 512 @ Keyboard input size max
 
 	.data
-szTop: 	.asciz 	"Group: Rasm 16|Andrew Barth-Yi|Alex Au|\nClass: CS 3B\nLab: RASM3\nDate: 10/23/2020\n\n"	@ title card																		@ second user input
+szTop: 	.asciz 	"Group: Rasm 16|Andrew Barth-Yi|Alex Au|\nClass: CS 3B\nLab: RASM3\nDate: 10/23/2020\n\n"	@ title card	
+szEmp:	.skip 512																							@ empty string for output
 	.text			
 
 	.global _start			@ Provide program starting address to linker
 
 _start:
+
+	ldr 	r0, =szTop		@ load title card
+	bl 		putstring		@ display title card
+	
+	mov 	r1, #65			@ move A into character search buffer
+	
+	bl		String_indexOf	@ call string index function
+	
+	ldr		r1, =szEmp		@ load empty string into r1
+	bl 		intasc32		@ put the index into emptystring
+	
+	mov 	r0, r1			@ put index string into r0
+	
+	bl		putstring		@ display index to console
+	
 
 	mov 	r0, #0 		@ set exit status to 0
 	mov 	r7, #1		@ service command code to 1
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # RASM3
 this is alex and andrew's RASM3 project
 written in assembly
+
+building commands:
+as -g -o RASM3Driver.o RASM3Driver.s
+as -g -o String_indexOf_1.o String_indexOf_1.s
+ld -o RASM3Driver RASM3Driver.o String_indexOf_1.o barnett_12Feb20.a
