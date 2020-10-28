765ef9b04181e58a5739227c399def73fe580146
sonic757 <sonic757ragequit@gmail.com>
2020-10-27 17:37:07 -0700

    Update String_toLowerCase.s

--- String2src/String_toLowerCase.s
+++ String2src/String_toLowerCase.s
@@ -24,22 +24,16 @@ loop:
 	cmp 	r6, #0			@ check if we are done copying string
 	beq		end				@ jump to end if we are done copying
 	ldrb	r0, [r5], #1 	@ load byte of string, increment
-	cmp		r0, #97			@ check character to lowercase a
-	blt		lower				@ branch to lower if found to be uppercase
+	cmp  	r0, #32		@check space delimination
+	streqb r0, [r3], #1
+	subeq r6, 1
+	beq		loop
+	cmp   r0, 97
+	addlt	r0, #32
+	strb	r0, [r3], #1			@ store the character back to the string
 	sub		r6, #1			@ decrement length
 	b			loop
 
-lower:
-	cmp			r0, #65						@ check if within alphabet bounds
-	bgt			inBounds					@ if within bounds go to inBounds loop
-	strltb	r0, [r3], #1			@ store the character back to the string
-	b 			loop							@ branch back to loop
-
-inBounds:
-	add 		r0, #32						@add 32 to byte, making it lowercase
-	strltb	r0, [r3], #1			@ store the character back to the string
-	b				loop							@ branch back to loop
-	
 end:
 	mov 	r0, r4			@ move address to r0 for return
 
